835773590d4837bc1e3d39b63b3e42f278f95b16
Christine Collier <161957076+christinecollier@users.noreply.github.com>
2024-11-15 11:41:31 +0000

    Corrected carbon intensity and forecast chart labels in index.js. Refactored non-fetch functions to be synchronous.

--- src/index.js
+++ src/index.js
@@ -8,21 +8,11 @@ let currentMinute = date.slice(14, 16);
 console.log(`date is ${currentDate}`);
 console.log(`time is ${currentTime}`);
 const api_url = `https://api.carbonintensity.org.uk/intensity/date/${currentDate}`
-/*Get current time as a fraction of 48 hours
-let index;
-if (date.slice(11, 13) === '00') {
-  if (date.slice(14, 16) === '00' || parseInt(date.slice(14, 16)) < 30) {
-  index = 0;
-  } else {
-   index = 1;
-  }
-} 
-*/
 
 //Get index of latest data
 let timeIndex;
 async function getTimeIndex() {
-  const response = await fetch(api_url)
+  const response = await fetch(api_url);
   const dataArray = await response.json();
   const { data } = dataArray;
   //Get possible array indices by hour
@@ -42,43 +32,46 @@ async function getTimeIndex() {
     }
   }
   getData(dataArray, timeIndex);
+  return timeIndex;
 }
 setInterval(getTimeIndex(), 60000); 
 
 //Get api data for current time
-const xlabels = [];
-const intensityLabels = [];
-const forecastLabels = [];
-let previousData;
-let latestData;
+let previousDataObject;
+let latestDataObject;
 
-async function getData(dataArray, timeIndex) {
+function getData(dataArray, timeIndex) {
+  const xlabels = [];
+  const intensityLabels = [];
+  const forecastLabels = [];
   const { data } = dataArray;
-  console.log(`timeIndex is ${timeIndex}`)
-  console.log(data);
   const { from, to, intensity } = data[timeIndex];
   let fromTime = from.slice(11, 16);
   let toTime = to.slice(11, 16);
   for (let i = 0; i < data.length; i++) {
     let timeLabel = data[i]['from'].slice(11, 16);
-    let intensity = data[i]['actual'];
-    let forecast = data[i]['forecast'];
+    let intensity = data[i]['intensity']['actual'];
+    let forecast = data[i]['intensity']['forecast'];
     xlabels.push(timeLabel);
-    intensityLabels.push(intensity);
-    forecastLabels.push(forecast);
+    if (intensity !== null) {
+      intensityLabels.push(intensity);
+    }
+    if (forecast !== null) {
+      forecastLabels.push(forecast);
+    }
   }
 
   document.getElementById('timePeriod').innerHTML = `From ${fromTime} to ${toTime}`
   document.getElementById('intensity').innerHTML = `Intensity: ${intensity['actual']} (${intensity['index']})`
-  latestData = data[timeIndex];
-  console.log(latestData);
-  console.log(xlabels);
-  return latestData, xlabels, ylabels;
+  previousDataObject = data[timeIndex - 1]
+  latestDataObject = data[timeIndex];
+  initialiseChart(xlabels, intensityLabels);
+  return previousDataObject, latestDataObject;
 }
 
 //Initialise chart.js
 initialiseChart(xlabels, intensityLabels);
-async function initialiseChart(xlabels, intensityLabels) {
+function initialiseChart(xlabels, intensityLabels) {
   const ctx = document.getElementById('chart');
   const myChart = new Chart(ctx, {
     type: 'line',
